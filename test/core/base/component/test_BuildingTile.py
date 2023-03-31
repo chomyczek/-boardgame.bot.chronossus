3b16b31bfce39cbccec41bc974760e3d527ab9c4
chomyczek <kaszubowski.adam@gmail.com>
2023-03-31 23:06:04 +0200

    add base component tests

---
+++ test/core/base/component/test_BuildingTile.py
@@ -0,0 +1,11 @@
+from src.core.base.component.BuildingTile import BuildingTile
+from src.core.base.type import BuildingType
+
+
+class TestBuildingTile:
+    def test_init(self):
+        score = 123
+        building_type = BuildingType.POWER_PLANT
+        building = BuildingTile(building_type, score)
+        assert building.type == building_type
+        assert building.points == score
